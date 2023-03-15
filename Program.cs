81c46b42ca440e79186a7afdd7a4007c171b6d29
Saurov Gleb <saurov.gleb@gmail.com>
2023-03-15 12:59:06 +0300

    Добавлена программа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+string[] array1 = new string[6] {"hello", "geekbrains", "777", "hi", "good luck", "lesson"};
+string[] array2 = new string[array1.Length];
+void ArraySecond(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArraySecond(array1, array2);
+PrintArray(array2);
